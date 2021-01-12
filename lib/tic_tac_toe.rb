2b92c7faa4faa0d618e97bed2595c22f608500d4
Elliott Vargo <vargoelliott@gmail.com>
2021-01-12 00:38:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,8 +89,8 @@ end
 
 
 
-def move(board, index, char)
-    board[index] = char
+def move(board, index, current_player(board))
+    board[index] = current_player(board)
 end
 
 
@@ -126,11 +126,11 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  char = "X"
+
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, char)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
