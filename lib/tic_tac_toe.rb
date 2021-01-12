6bd8419ccad67abf21231d5298ca2eb927894837
Elliott Vargo <vargoelliott@gmail.com>
2021-01-12 01:13:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -132,6 +132,9 @@ def turn(board)
   if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
+    over?(board)
+    won?(board)
+    draw?(board)
   else
     turn(board)
   end
